e1cdec393dfebc44c62d0cd4f9e4e23d86d3b373
Xari <31975280+deerfoster@users.noreply.github.com>
2026-06-12 19:36:43 -0400

    .

---
+++ ModuloReFun/ModuloReFun/Memoria.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModuloReFun
+{
+    internal class Memoria
+    {
+        public TimeSpan nuevaHora;
+        public bool alarmaActiva = false;
+        public bool sonidoAlarma = false;
+
+        private static Memoria singleton;
+
+        private Memoria(){
+        }
+        public static Memoria Instancia
+        {
+            get
+            {
+                if (singleton == null)
+                {
+                    singleton = new Memoria();
+                }
+                return singleton;
+            }
+        }
+
+        public void RevisarAlarma(TimeSpan horaActual)
+        {
+            if (alarmaActiva == true)
+            {
+                if (horaActual.Hours == nuevaHora.Hours && horaActual.Minutes == nuevaHora.Minutes)
+                {
+                    sonidoAlarma = true;
+                    new System.Media.SoundPlayer(Properties.Resources.alarma).PlayLooping();
+                }
+            }
+        }
+    }
+}
